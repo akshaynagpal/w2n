c571bae29f079de413ad33c01c4d06a252043f0f
Akshay Nagpal <akshay2626@gmail.com>
2015-12-30 04:15:02 +0530

    code rewritten

--- w2n.py
+++ w2n.py
@@ -1,5 +1,3 @@
-import math
-
 american_number_system = {
     'zero': 0,
     'one': 1,
@@ -63,46 +61,78 @@ testing for american_number_system
 'thirty five thousand seven hundred eight nine' : 30,5,1000,7,100,80,9 ->
 'thirty five thousand seven hundred ninety': 30,5,1000,7,100,90
 'three hundred thousand four hundred forty two': 3,100,1000,4,100,40,2
-
-algo -
-BEGIN
-1. start from left
-2. initialize total_sum and temp_sum to zero.
-3. Start adding numbers to temp_sum till you reach a 1000,100,100000 etc.
-4. when reach 1000,1000000 etc. multiply temp_sum with 100, 1000 whatever reached.
-5. add this to total sum and reset temp_sum=0.
-6. if(you reach end and temp_sum does not include 100):
-7. 			add temp_sum to total_sum.
-END
 """
 ########################################################################################
 
+
+
 def word_to_num(number_sentence):
     split_words = number_sentence.split()  # split sentence into words
-    separate_numbers = []  # map words to numbers through number system dictionaries
+    clean_numbers = []  # removing and, & etc.
     for word in split_words:
         if word in american_number_system:
-            separate_numbers.append(american_number_system[word])
-    number_formation(separate_numbers)
-    return 0
+            clean_numbers.append(word)
 
+    billion_index = clean_numbers.index('billion') if 'billion' in clean_numbers else -1
+    million_index = clean_numbers.index('million') if 'million' in clean_numbers else -1
+    thousand_index = clean_numbers.index('thousand') if 'thousand' in clean_numbers else -1
 
-def number_formation(separate_numbers):
-    print separate_numbers
     total_sum = 0
-    temp_sum = 0
-    for number in separate_numbers:
-        if (math.log10(number)).is_integer() and (math.log10(number)) != 0:
-            if temp_sum == 0:
-                total_sum += number
-            else:
-                total_sum += (number * temp_sum)
-                temp_sum = 0
+    if billion_index>-1 :
+        billion_multiplier = number_formation(clean_numbers[0:billion_index])
+        # print "billion_multiplier",str(billion_multiplier)
+        total_sum += billion_multiplier * 1000000000
+
+    if million_index>-1 :
+        if billion_index>-1:
+            million_multiplier = number_formation(clean_numbers[billion_index+1:million_index])
         else:
-            temp_sum += number
-    total_sum += temp_sum
+            million_multiplier = number_formation(clean_numbers[0:million_index])
+        total_sum += million_multiplier * 1000000
+        # print "million_multiplier",str(million_multiplier)
+
+    if thousand_index > -1:
+        if million_index > -1:
+            thousand_multiplier = number_formation(clean_numbers[million_index+1:thousand_index])
+        elif billion_index>-1 and million_index==-1:
+            thousand_multiplier = number_formation(clean_numbers[billion_index+1:thousand_index])
+        else:
+            thousand_multiplier = number_formation(clean_numbers[0:thousand_index])
+        total_sum += thousand_multiplier * 1000
+        # print "thousand_multiplier",str(thousand_multiplier)
+
+    if thousand_index>-1 and thousand_index != len(clean_numbers)-1:
+        hundreds = number_formation(clean_numbers[thousand_index+1:])
+    elif million_index>-1 and million_index != len(clean_numbers)-1:
+        hundreds = number_formation(clean_numbers[million_index+1:])
+    elif billion_index>-1 and billion_index != len(clean_numbers)-1:
+        hundreds = number_formation(clean_numbers[billion_index+1:])
+    elif thousand_index==-1 and million_index==-1 and billion_index==-1:
+        hundreds = number_formation(clean_numbers)
+    else:
+        hundreds = 0
+    total_sum += hundreds
+    # print "hundreds",str(hundreds)
+        
     print total_sum
-    return 0
+    return total_sum
+
+
+def number_formation(number_words):
+    numbers = []
+    for number_word in number_words:
+        numbers.append(american_number_system[number_word])
+    if len(numbers)==4:
+        return (numbers[0]*numbers[1])+numbers[2]+numbers[3]
+    elif len(numbers)==3:
+        return numbers[0]*numbers[1] + numbers[2]
+    elif len(numbers)==2:
+        if 100 in numbers:
+            return numbers[0]*numbers[1]
+        else:
+            return numbers[0]+numbers[1]
+    else:
+        return numbers[0]
 
 word_to_num('one hundred and forty two')
 
@@ -111,3 +141,17 @@ word_to_num('three thousand four hundred and seventy four')
 word_to_num('thirty five thousand seven hundred eighty nine')
 
 word_to_num('three hundred thousand four hundred forty two')
+
+word_to_num('three million') ##
+
+word_to_num('three billion') ##
+
+word_to_num('three thousand') ##
+
+word_to_num('three hundred thousand four hundred') ##
+
+word_to_num('thirty five thousand seven hundred eighty one')
+
+word_to_num('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty one')
+
+word_to_num('four hundred and forty three thousand') ##
