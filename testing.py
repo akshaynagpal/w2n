591f424b9be99a6c5aeed1ec7492b3feb2cba465
Akshay Nagpal <akshay2626@gmail.com>
2015-12-31 02:45:55 +0530

    testing file separated

---
+++ testing.py
@@ -0,0 +1,25 @@
+""" testing....
+
+word_to_num('one hundred and forty two')
+
+word_to_num('three thousand four hundred and seventy four')
+
+word_to_num('thirty five thousand seven hundred eighty nine')
+
+word_to_num('three hundred thousand four hundred forty two')
+
+word_to_num('three million') ##
+
+word_to_num('three billion') ##
+
+word_to_num('three thousand') ##
+
+word_to_num('three hundred thousand four hundred') ##
+
+word_to_num('thirty five thousand seven hundred eighty one')
+
+word_to_num('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty one')
+
+word_to_num('four hundred and forty three thousand') ##
+
+"""
