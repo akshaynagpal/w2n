ad2b988ccc4430e78b668f4c47f56b04174390ce
Akshay Nagpal <akshay2626@gmail.com>
2017-05-16 22:59:22 -0400

    fixed issue #7

--- unit_testing.py
+++ unit_testing.py
@@ -1,18 +1,33 @@
 import unittest
 from word2number import w2n
 
+
 class TestW2N(unittest.TestCase):
 
-	def test_output(self):
-		self.assertEqual(w2n.word_to_num("two million three thousand nine hundred and eighty four"),2003984)
-		self.assertEqual(w2n.word_to_num("nineteen"),19)
-		self.assertEqual(w2n.word_to_num('three billion'),3000000000)
-		self.assertEqual(w2n.word_to_num('three million'),3000000)
-		self.assertEqual(w2n.word_to_num('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty nine')
-,123456789)
-		self.assertEqual(w2n.word_to_num('eleven'),11)
-		self.assertEqual(w2n.word_to_num('nineteen billion and nineteen'),19000000019)
-		self.assertEqual(w2n.word_to_num('one hundred and forty two'),142)
+    def test_positives(self):
+        self.assertEqual(w2n.word_to_num("two million three thousand nine hundred and eighty four"), 2003984)
+        self.assertEqual(w2n.word_to_num("nineteen"), 19)
+        self.assertEqual(w2n.word_to_num("two thousand and nineteen"), 2019)
+        self.assertEqual(w2n.word_to_num("two million three thousand and nineteen"), 2003019)
+        self.assertEqual(w2n.word_to_num('three billion'), 3000000000)
+        self.assertEqual(w2n.word_to_num('three million'), 3000000)
+        self.assertEqual(w2n.word_to_num('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty nine')
+, 123456789)
+        self.assertEqual(w2n.word_to_num('eleven'), 11)
+        self.assertEqual(w2n.word_to_num('nineteen billion and nineteen'), 19000000019)
+        self.assertEqual(w2n.word_to_num('one hundred and forty two'), 142)
+        self.assertEqual(w2n.word_to_num('112'), 112)
+        self.assertEqual(w2n.word_to_num('11211234'), 11211234)
+        self.assertEqual(w2n.word_to_num('five'), 5)
+        self.assertEqual(w2n.word_to_num('two million twenty three thousand and forty nine'), 2023049)
 
+    def test_negatives(self):
+        self.assertEqual(w2n.word_to_num('112-'), 'Error: No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('-'), 'Error: No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('on'), 'Error: No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('million million'), 'Error: Redundant number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('three million million'), 'Error: Redundant number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('million four million'), 'Error: Redundant number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
+        self.assertEqual(w2n.word_to_num('thousand million'), 'Error: Malformed number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)')
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- word2number/w2n.py
+++ word2number/w2n.py
@@ -32,6 +32,7 @@ american_number_system = {
     'million': 1000000,
     'billion': 1000000000
 }
+
 """
 #TODO
 indian_number_system = {
@@ -46,6 +47,15 @@ indian_number_system = {
     'eight': 8,
     'nine': 9,
     'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
     'twenty': 20,
     'thirty': 30,
     'forty': 40,
@@ -61,30 +71,52 @@ indian_number_system = {
     'crore': 10000000
 }
 """
+
+
+"""
+method to return integer for an input `number_sentence` string
+"""
+
+
 def word_to_num(number_sentence):
-    number_sentence = number_sentence.lower()
+    number_sentence = number_sentence.lower()  # converting input to lowercase
+
+    if(number_sentence.isdigit()):  # return the number if user enters a number string
+        return int(number_sentence)
+
     split_words = number_sentence.split()  # split sentence into words
-    clean_numbers = []  # removing and, & etc.
+
+    clean_numbers = []
+
+    # removing and, & etc.
     for word in split_words:
         if word in american_number_system:
             clean_numbers.append(word)
 
-    # Error message if the user enters invalid input!        
+    # Error message if the user enters invalid input!
     if len(clean_numbers) == 0:
-        return "Error: Please enter a valid number word (eg. two million twenty three thousand and forty nine) "
+        return "Error: No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)"
+
+    # Error if user enters million,billion, thousand twice
+    if clean_numbers.count('thousand') > 1 or clean_numbers.count('million') > 1 or clean_numbers.count('billion') > 1:
+        return "Error: Redundant number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)"
 
     billion_index = clean_numbers.index('billion') if 'billion' in clean_numbers else -1
     million_index = clean_numbers.index('million') if 'million' in clean_numbers else -1
     thousand_index = clean_numbers.index('thousand') if 'thousand' in clean_numbers else -1
 
-    total_sum = 0
-    if billion_index>-1 :
+    if (thousand_index > -1 and (thousand_index < million_index or thousand_index < billion_index)) or (million_index>-1 and million_index < billion_index):
+        return "Error: Malformed number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)"
+
+    total_sum = 0  # storing the number to be returned
+
+    if billion_index > -1:
         billion_multiplier = number_formation(clean_numbers[0:billion_index])
         # print "billion_multiplier",str(billion_multiplier)
         total_sum += billion_multiplier * 1000000000
 
-    if million_index>-1 :
-        if billion_index>-1:
+    if million_index > -1:
+        if billion_index > -1:
             million_multiplier = number_formation(clean_numbers[billion_index+1:million_index])
         else:
             million_multiplier = number_formation(clean_numbers[0:million_index])
@@ -94,20 +126,20 @@ def word_to_num(number_sentence):
     if thousand_index > -1:
         if million_index > -1:
             thousand_multiplier = number_formation(clean_numbers[million_index+1:thousand_index])
-        elif billion_index>-1 and million_index==-1:
+        elif billion_index > -1 and million_index == -1:
             thousand_multiplier = number_formation(clean_numbers[billion_index+1:thousand_index])
         else:
             thousand_multiplier = number_formation(clean_numbers[0:thousand_index])
         total_sum += thousand_multiplier * 1000
         # print "thousand_multiplier",str(thousand_multiplier)
 
-    if thousand_index>-1 and thousand_index != len(clean_numbers)-1:
+    if thousand_index > -1 and thousand_index != len(clean_numbers)-1:
         hundreds = number_formation(clean_numbers[thousand_index+1:])
-    elif million_index>-1 and million_index != len(clean_numbers)-1:
+    elif million_index > -1 and million_index != len(clean_numbers)-1:
         hundreds = number_formation(clean_numbers[million_index+1:])
-    elif billion_index>-1 and billion_index != len(clean_numbers)-1:
+    elif billion_index > -1 and billion_index != len(clean_numbers)-1:
         hundreds = number_formation(clean_numbers[billion_index+1:])
-    elif thousand_index==-1 and million_index==-1 and billion_index==-1:
+    elif thousand_index == -1 and million_index == -1 and billion_index == -1:
         hundreds = number_formation(clean_numbers)
     else:
         hundreds = 0
@@ -116,18 +148,26 @@ def word_to_num(number_sentence):
     return total_sum
 
 
+"""
+function to form numeric multipliers for million, billion, thousand etc.
+
+input: list of strings
+return value: integer
+"""
+
+
 def number_formation(number_words):
     numbers = []
     for number_word in number_words:
         numbers.append(american_number_system[number_word])
-    if len(numbers)==4:
-        return (numbers[0]*numbers[1])+numbers[2]+numbers[3]
-    elif len(numbers)==3:
-        return numbers[0]*numbers[1] + numbers[2]
-    elif len(numbers)==2:
+    if len(numbers) == 4:
+        return (numbers[0] * numbers[1]) + numbers[2] + numbers[3]
+    elif len(numbers) == 3:
+        return numbers[0] * numbers[1] + numbers[2]
+    elif len(numbers) == 2:
         if 100 in numbers:
-            return numbers[0]*numbers[1]
+            return numbers[0] * numbers[1]
         else:
-            return numbers[0]+numbers[1]
+            return numbers[0] + numbers[1]
     else:
         return numbers[0]
