4c538b34687aa08f6a57160f8eeb0ef74c884b6e
Sͬeͥbͭaͭsͤtͬian <sebastian.ritter@example.com>
2020-09-04 17:57:05 +0200

    changed setup for data files

--- setup.py
+++ setup.py
@@ -11,18 +11,29 @@ def open_file(fname):
 
 setup(
   name = 'word2number-i18n',
-  packages = ['word2numberi18n'],  # this must be the same as the name above
+  include_package_data=True,
+  packages = ['word2numberi18n','word2numberi18n/data'],  # this must be the same as the name above
+  package_data={
+    'word2numberi18n/data': ["*.txt"],
+  },
   version = '0.1',
   license=open('LICENSE.txt').read(),
   description = 'Convert i18n number words eg. three hundred and forty two to numbers (342).',
   author = 'Sebastian Ritter',
+  python_requires='>=3',
   author_email = 'nospam@example.com',
   url = 'https://github.com/bastie/w2ni18n',  # use the URL to the github repo
-  download_url = 'https://github.com/akshaynagpal/w2n/tarball/1.1', 
+  project_urls={
+      'Source': 'https://github.com/bastie/w2ni18n',
+  },
   keywords = ['numbers', 'convert', 'words', 'i18n'],  # arbitrary keywords
   classifiers = [
       'Intended Audience :: Developers',
-      'Programming Language :: Python'
+      'Programming Language :: Python',
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Topic :: Software Development :: Internationalization'
   ],
   long_description=open_file('README.rst').read()
 )
