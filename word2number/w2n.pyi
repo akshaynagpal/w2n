c8d636b1e50e716a68d32b5d7e775f315cc1768a
Ivan Kouznetsov <52215050+Ivan-Kouznetsov@users.noreply.github.com>
2021-05-11 21:44:22 -0700

    Add a mypy stub
    A mypy compatible stub file as per https://mypy.readthedocs.io/en/stable/stubs.html. Types annotations mirror the comments in w2n.py

---
+++ word2number/w2n.pyi
@@ -0,0 +1,10 @@
+from typing import List, Union
+
+def number_formation(number_words: List[str]) -> int:
+    pass
+
+def get_decimal_sum(decimal_digit_words: List[str]) -> float:
+    pass
+
+def word_to_num(number_sentence: str) -> Union[int, float, None]:
+    pass
