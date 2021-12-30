9ad46323b3d944b8169d9cb046786119813c4e9a
Tin Wizard <11970957+WaltWh@users.noreply.github.com>
2021-12-29 23:26:00 -0600

    Fixing a lot of bugs, massive refactoring, added function to replace numbers in-place in sentences

--- unit_testing.py
+++ unit_testing.py
@@ -1,39 +1,50 @@
 import unittest
+
 from word2number import w2n
+
+general_test_data = [("two million three thousand nine hundred and eighty four", 2003984),
+                     ("nineteen", 19),
+                     ("two thousand and nineteen", 2019),
+                     ("two million three thousand and nineteen", 2003019),
+                     ('three billion', 3000000000),
+                     ('three million', 3000000),
+                     ('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty nine',
+                      123456789),
+                     ('eleven', 11),
+                     ('nineteen billion and nineteen', 19000000019), ('one hundred and forty two', 142),
+                     ('112', 112),
+                     ('11211234', 11211234),
+                     ('five', 5),
+                     ('two million twenty three thousand and forty nine', 2023049),
+                     ('two point three', 2.3),
+                     ('two million twenty three thousand and forty nine point two three six nine', 2023049.2369),
+                     ('one billion two million twenty three thousand and forty nine point two three six nine',
+                      1002023049.2369),
+                     ('point one', 0.1),
+                     ('point', 0),
+                     ('point nineteen', 0),
+                     ('one hundred thirty-five', 135),
+                     ('hundred', 100),
+                     ('thousand', 1000),
+                     ('million', 1000000),
+                     ('billion', 1000000000),
+                     ('nine point nine nine nine', 9.999),
+                     ('a hundred and fourteen', 114),
+                     ('three thousand, one hundred and forty-seven', 3147),
+                     ('forty-five hundred', 4500)]
+
+sentence_test_data = [(
+    'A hundred and twelve years, two hundred and twenty-one days, thirteen hours, and twenty-two minutes',
+    '112 years, 221 days, 13 hours, and 22 minutes'),
+    ('4 hours, ten days, 7 minutes, 6 seconds', '4 hours, 10 days, 7 minutes, 6 seconds')]
 
 
 class TestW2N(unittest.TestCase):
-    def test_positives(self):
-        self.assertEqual(w2n.word_to_num("two million three thousand nine hundred and eighty four"), 2003984)
-        self.assertEqual(w2n.word_to_num("nineteen"), 19)
-        self.assertEqual(w2n.word_to_num("two thousand and nineteen"), 2019)
-        self.assertEqual(w2n.word_to_num("two million three thousand and nineteen"), 2003019)
-        self.assertEqual(w2n.word_to_num('three billion'), 3000000000)
-        self.assertEqual(w2n.word_to_num('three million'), 3000000)
-        self.assertEqual(w2n.word_to_num('one hundred twenty three million four hundred fifty six thousand seven hundred and eighty nine')
-, 123456789)
-        self.assertEqual(w2n.word_to_num('eleven'), 11)
-        self.assertEqual(w2n.word_to_num('nineteen billion and nineteen'), 19000000019)
-        self.assertEqual(w2n.word_to_num('one hundred and forty two'), 142)
-        self.assertEqual(w2n.word_to_num('112'), 112)
-        self.assertEqual(w2n.word_to_num('11211234'), 11211234)
-        self.assertEqual(w2n.word_to_num('five'), 5)
-        self.assertEqual(w2n.word_to_num('two million twenty three thousand and forty nine'), 2023049)
-        self.assertEqual(w2n.word_to_num('two point three'), 2.3)
-        self.assertEqual(w2n.word_to_num('two million twenty three thousand and forty nine point two three six nine'), 2023049.2369)
-        self.assertEqual(w2n.word_to_num('one billion two million twenty three thousand and forty nine point two three six nine'), 1002023049.2369)
-        self.assertEqual(w2n.word_to_num('point one'), 0.1)
-        self.assertEqual(w2n.word_to_num('point'), 0)
-        self.assertEqual(w2n.word_to_num('point nineteen'), 0)
-        self.assertEqual(w2n.word_to_num('one hundred thirty-five'), 135)
-        self.assertEqual(w2n.word_to_num('hundred'), 100)
-        self.assertEqual(w2n.word_to_num('thousand'), 1000)
-        self.assertEqual(w2n.word_to_num('million'), 1000000)
-        self.assertEqual(w2n.word_to_num('billion'), 1000000000)
-        self.assertEqual(w2n.word_to_num('nine point nine nine nine'), 9.999)
-        self.assertEqual(w2n.word_to_num('seventh point nineteen'), 0)
+    def test_word_to_num_positives(self):
+        for datapoint in general_test_data:
+            self.assertEqual(datapoint[1], w2n.word_to_num(datapoint[0]))
 
-    def test_negatives(self):
+    def test_word_to_num_negatives(self):
         self.assertRaises(ValueError, w2n.word_to_num, '112-')
         self.assertRaises(ValueError, w2n.word_to_num, '-')
         self.assertRaises(ValueError, w2n.word_to_num, 'on')
@@ -41,8 +52,16 @@ class TestW2N(unittest.TestCase):
         self.assertRaises(ValueError, w2n.word_to_num, 'three million million')
         self.assertRaises(ValueError, w2n.word_to_num, 'million four million')
         self.assertRaises(ValueError, w2n.word_to_num, 'thousand million')
-        self.assertRaises(ValueError, w2n.word_to_num, 'one billion point two million twenty three thousand and forty nine point two three six nine')
+        self.assertRaises(ValueError, w2n.word_to_num,
+                          'one billion point two million twenty three thousand and forty nine point two three six nine')
         self.assertRaises(ValueError, w2n.word_to_num, 112)
+
+    def test_numwords_in_sentence(self):
+        for datapoint in general_test_data:
+            self.assertEqual(str(datapoint[1]), w2n.numwords_in_sentence(datapoint[0]))
+        for datapoint in sentence_test_data:
+            self.assertEqual(datapoint[1], w2n.numwords_in_sentence(datapoint[0]))
+
 
 if __name__ == '__main__':
     unittest.main()
